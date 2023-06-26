bb46a6f8f92f862a78dc76a424242677503a3653
Rodrigo <rodrigofernandes@alu.ufc.br>
2023-06-26 16:52:51 -0300

    substitui o parametro - 1

--- src/model/Emprestimo.java
+++ src/model/Emprestimo.java
@@ -12,18 +12,18 @@ package model;
 public class Emprestimo {
     
     private int id_emprestimo;
-    private int id_cliente;
-    private int id_livro;
+    private Cliente cliente;
+    private Livro livro;
     private String data_emprestimo;
     private String data_devolucao;
 
     public Emprestimo() {
-    }   
+    }
 
-    public Emprestimo(int id_emprestimo, int id_cliente, int id_livro, String data_emprestimo, String data_devolucao) {
+    public Emprestimo(int id_emprestimo, Cliente cliente, Livro livro, String data_emprestimo, String data_devolucao) {
         this.id_emprestimo = id_emprestimo;
-        this.id_cliente = id_cliente;
-        this.id_livro = id_livro;
+        this.cliente = cliente;
+        this.livro = livro;
         this.data_emprestimo = data_emprestimo;
         this.data_devolucao = data_devolucao;
     }
@@ -36,20 +36,20 @@ public class Emprestimo {
         this.id_emprestimo = id_emprestimo;
     }
 
-    public int getId_cliente() {
-        return id_cliente;
+    public Cliente getCliente() {
+        return cliente;
     }
 
-    public void setId_cliente(int id_cliente) {
-        this.id_cliente = id_cliente;
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 
-    public int getId_livro() {
-        return id_livro;
+    public Livro getLivro() {
+        return livro;
     }
 
-    public void setId_livro(int id_livro) {
-        this.id_livro = id_livro;
+    public void setLivro(Livro livro) {
+        this.livro = livro;
     }
 
     public String getData_emprestimo() {
@@ -67,5 +67,55 @@ public class Emprestimo {
     public void setData_devolucao(String data_devolucao) {
         this.data_devolucao = data_devolucao;
     }
-    
 }
+
+public class Cliente {
+    
+    private int id;
+    private String nome;
+    private String dataNasc;
+    private String sexo;
+    private String cpf;
+    private String endereco;
+    private String fone;
+
+    public Cliente() {
+    }   
+
+    public Cliente(String nome, String dataNasc, String sexo, String cpf, String endereco, String fone) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.fone = fone;
+    }
+
+    // Getters e Setters omitidos para brevidade
+    
+    public boolean possuiDadosIncompletos() {
+        return nome.isEmpty() || dataNasc.isEmpty() || sexo.isEmpty() || cpf.isEmpty() || endereco.isEmpty();
+    }
+}
+
+public class Livro {
+    
+    private int id;
+    private String exemplar;
+    private String autor;
+    private int edicao;
+    private int ano;
+    private String disponibilidade;
+
+    public Livro() {
+    }   
+
+    public Livro(String exemplar, String autor, int edicao, int ano, String disponibilidade) {
+        this.exemplar = exemplar;
+        this.autor = autor;
+        this.edicao = edicao;
+        this.ano = ano;
+        this.disponibilidade = disponibilidade;
+    }
+
+    // Getters e Setters omitidos para bre
